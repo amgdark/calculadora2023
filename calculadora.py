cf757e828c1225d8c5c294831259810522b02d7f
Alejandro Mauricio González <amgdark@uaz.edu.mx>
2023-02-01 13:33:42 -0600

    Agrega calculadora

---
+++ calculadora.py
@@ -0,0 +1,4 @@
+class Calculadora:
+    
+    def sumar(self, num1, num2):
+        return num1 + num2
---
+++ test_calculadora.py
@@ -0,0 +1,11 @@
+import unittest
+from calculadora import Calculadora
+
+class TestCalculadora(unittest.TestCase):
+    
+    def test_2_mas_2(self):
+        calc = Calculadora()
+        self.assertEqual(4, calc.sumar(2, 2))
+        
+if __name__ == '__main__':
+    unittest.main()
