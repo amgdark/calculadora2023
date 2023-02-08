b15217c5650ae5194a585e9479a64b0633bafc32
Alejandro Mauricio González <amgdark@uaz.edu.mx>
2023-02-08 14:01:32 -0600

    Prueba de 10 mas 2

--- test_calculadora.py
+++ test_calculadora.py
@@ -7,5 +7,9 @@ class TestCalculadora(unittest.TestCase):
         calc = Calculadora()
         self.assertEqual(4, calc.sumar(2, 2))
         
+    def test_10_mas_2(self):
+        calc = Calculadora()
+        self.assertEqual(11, calc.sumar(10, 2))
+        
 if __name__ == '__main__':
     unittest.main()
